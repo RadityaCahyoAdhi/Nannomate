83db91be3150b6a91e30b51f134e362eddafcf8b
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2021-07-11 15:01:49 +0700

    updated comments, made index function accessible to all authenticated user, added id_umur awal not greater than id_umur_akhir check on store function

--- backend-nannomate/app/Http/Controllers/SpesiesController.php
+++ backend-nannomate/app/Http/Controllers/SpesiesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use App\Models\spesies_nanofosil;
 use App\Models\zona_geologi;
-use App\Models\umur_geologi;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,40 +15,36 @@ class SpesiesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    // public function index() {
-    //     $daftarSpesies = spesies_nanofosil::all();
-
-    //     return response()->json($daftarSpesies, 200);
-    // }
-
+    //menampilkan daftar spesies nannofosil terverifikasi
     public function index() {
-        $user = Auth::user();
-        if ($user['role'] != 'user login') {
-            return response()->json(['error'=>'Unauthorised'], 401);
-        } else {
-            $daftarSpesiesTerverifikasi = spesies_nanofosil::where('status', '=', 'terverifikasi')->get();
+        $daftarSpesiesTerverifikasi = spesies_nanofosil::where('status', '=', 'terverifikasi')->get();
 
-            return response()->json($daftarSpesiesTerverifikasi, 200);
-        }
+        return response()->json($daftarSpesiesTerverifikasi, 200);
     }
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    //menambahkan spesies nannofosil terverifikasi baru
     public function store(Request $request)
     {
+        //mengambil data user
         $user = Auth::user();
+
+        //memastikan user memiliki role admin
         if ($user['role'] != 'admin') {
             return response()->json(['error'=>'Unauthorised'], 401);
         } else {
-            //find spesies terverifikasi same or new
+            //memastikan spesies terverifikasi yang akan disimpan sudah ada di database atau belum
             $hit = spesies_nanofosil::where('nama_spesies', '=', $request->nama_spesies)->where('status', '=', 'terverifikasi')->get();
             $hit = $hit->count();
             if ($hit > 0) {
                 return response()->json(['error'=>'Spesies already exists!'], 404);
             } else {
+                //memastikan semua isian request yang dibutuhkan terisi
                 $validator = Validator::make($request->all(), [
                     'nama_spesies' => 'required',
                     'id_umur_awal' => 'required',
@@ -60,16 +55,26 @@ class SpesiesController extends Controller
                     return response()->json(['error'=>$validator->errors()], 401);
                 }
 
+                //memastikan request id_umur_awal tidak lebih dari id_umur_akhir
+                if ($request->id_umur_awal > $request->id_umur_akhir) {
+                    return response()->json(['error' => 'Umur awal melebihi umur akhir'], 401);
+                }
+
+                //mengumpulkan seluruh request
                 $input = $request->all();
+
+                //menyimpan record nama_spesies dan status baru ke tabel database 'spesies_nanofosil'
                 $spesies_nanofosil = spesies_nanofosil::create([
                     'nama_spesies' => $input['nama_spesies'],
                     'status' => 'terverifikasi'
                 ]);
 
+                //mengumpulkan data untuk record table database zona_geologi
                 $input_zona_geologi['id_spesies'] = $spesies_nanofosil['id_spesies'];
                 $umur_awal = $input['id_umur_awal'];
                 $umur_akhir= $input['id_umur_akhir'];
 
+                //menyimpan record id_spesies dan id_umur dari spesies terverifikasi baru ke tabel database 'zona_geologi'
                 for ($i = $umur_awal; $i <= $umur_akhir; $i++) {
                     $input_zona_geologi['id_umur'] = $i;
                     zona_geologi::create([
@@ -88,6 +93,7 @@ class SpesiesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    //menampilkan salah satu spesies nannofosil beserta id-id umurnya
     public function show($id)
     {
         $spesies_nanofosil['spesies_nanofosil'] = spesies_nanofosil::find($id);
@@ -103,16 +109,22 @@ class SpesiesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    //mengedit suatu spesies nannofosil
     public function update(Request $request, $id)
     {
+        //mengambil data user
         $user = Auth::user();
+
+        //memastikan user memiliki role admin
         if ($user['role'] != 'admin') {
             return response()->json(['error'=>'Unauthorised'], 401);
         } else {
+            //memeriksa apakah id_spesies request ada di database
             $spesies_nanofosil = spesies_nanofosil::find($id);
             if (is_null($spesies_nanofosil)){
                 return response()->json(['error'=>'Data Not Found!'], 404);
             }
+            //memastikan semua isian request yang dibutuhkan terisi
             $validator = Validator::make($request->all(), [
                 'nama_spesies' => 'required',
                 'id_umur_awal' => 'required',
@@ -123,21 +135,28 @@ class SpesiesController extends Controller
                 return response()->json(['error'=>$validator->errors()], 401);
             }
 
+            //memastikan request id_umur_awal tidak lebih dari id_umur_akhir
             if ($request->id_umur_awal > $request->id_umur_akhir) {
                 return response()->json(['error' => 'Umur awal melebihi umur akhir'], 401);
             }
 
+            //mengumpulkan seluruh request
             $input = $request->all();
+
+            //mengedit record nama_spesies pada tabel database 'spesies_nanofosil' sesuai input id
             spesies_nanofosil::where('id_spesies', $id)->update([
                 'nama_spesies' => $input['nama_spesies']
             ]);
 
+            //menghapus zona_geologi lama dari spesies yang diedit
             zona_geologi::where('id_spesies', '=', $id)->delete();
 
+            //mengumpulkan data untuk record table database zona_geologi
             $input_zona_geologi['id_spesies'] = $id;
             $umur_awal = $input['id_umur_awal'];
             $umur_akhir= $input['id_umur_akhir'];
 
+            //menyimpan record id_spesies dan id_umur dari spesies yang diedit ke tabel database 'zona_geologi'
             for ($i = $umur_awal; $i <= $umur_akhir; $i++) {
                 $input_zona_geologi['id_umur'] = $i;
                 zona_geologi::create([
@@ -155,17 +174,23 @@ class SpesiesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    //menghapus suatu spesies nannofosil
     public function destroy($id)
     {
+        //mengambil data user
         $user = Auth::user();
+
+        //memastikan user memiliki role admin
         if ($user['role'] != 'admin') {
             return response()->json(['error'=>'Unauthorised'], 401);
         } else {
+            //memeriksa apakah id_spesies request ada di database
             $spesies_nanofosil = spesies_nanofosil::find($id);
             if (is_null($spesies_nanofosil)){
                 return response()->json(['error'=>'Data Not Found!'], 404);
             }
 
+            //menghapus record pada tabel database zona_geologi dan spesies_nannofosil yang berhubungan dengan id_spesies request
             $zona_geologi = zona_geologi::where('id_spesies', $id);
             $zona_geologi->delete();
             $spesies_nanofosil->delete();
