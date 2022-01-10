503dd1674b4c075876396a85f86c7cd21686d95e
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-01-10 22:55:14 +0700

    added 'kelimpahan', 'preparasi', and 'pengawetan' values validation in ExcelRequestInputController

--- backend-nannomate/app/Http/Controllers/ExcelRequestInputController.php
+++ backend-nannomate/app/Http/Controllers/ExcelRequestInputController.php
@@ -32,9 +32,9 @@ class ExcelRequestInputController extends Controller
                 'latitude' => 'required',
                 //sample table
                 'kode_sample' => 'required',
-                'kelimpahan' => 'required',
-                'preparasi' => 'required',
-                'pengawetan' => 'required',
+                'kelimpahan' => 'required|in:Kosong,Jarang,Beberapa,Umum,Melimpah',
+                'preparasi' => 'required|in:Ayakan,Asahan,Smear,Lain',
+                'pengawetan' => 'required|in:Jelek,Sedang,Bagus',
                 'tujuan' => 'required',
                 'stopsite' => 'required'
             ]);
