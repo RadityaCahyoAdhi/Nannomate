b59190ba208d11daff45e927ce9b26963d41ae33
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-01-11 15:52:36 +0700

    added 'kelimpahan', 'preparasi', and 'pengawetan' values validation to 'store' and 'update' function in DetailSampelController

--- backend-nannomate/app/Http/Controllers/DetailSampelController.php
+++ backend-nannomate/app/Http/Controllers/DetailSampelController.php
@@ -48,9 +48,9 @@ class DetailSampelController extends Controller
                 'latitude' => 'required',
                 //sample table
                 'kode_sample' => 'required',
-                'kelimpahan' => 'required',
-                'preparasi' => 'required',
-                'pengawetan' => 'required',
+                'kelimpahan' => 'required|in:Kosong,Jarang,Beberapa,Umum,Melimpah',
+                'preparasi' => 'required|in:Ayakan,Asahan,Smear,Lain',
+                'pengawetan' => 'required|in:Jelek,Sedang,Bagus',
                 'tujuan' => 'required',
                 'stopsite' => 'required'
             ]);
@@ -226,9 +226,9 @@ class DetailSampelController extends Controller
                 'latitude' => 'required',
                 //sample table
                 'kode_sample' => 'required',
-                'kelimpahan' => 'required',
-                'preparasi' => 'required',
-                'pengawetan' => 'required',
+                'kelimpahan' => 'required|in:Kosong,Jarang,Beberapa,Umum,Melimpah',
+                'preparasi' => 'required|in:Ayakan,Asahan,Smear,Lain',
+                'pengawetan' => 'required|in:Jelek,Sedang,Bagus',
                 'tujuan' => 'required',
                 'stopsite' => 'required'
             ]);
