a0d4b143c04c6c37dd85d56b41cb1eac36abeabb
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-01-28 08:36:58 +0700

    deleted some unused codes

--- backend-nannomate/app/Http/Controllers/UmurGeologiController.php
+++ backend-nannomate/app/Http/Controllers/UmurGeologiController.php
@@ -26,17 +26,6 @@ class UmurGeologiController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -47,28 +36,5 @@ class UmurGeologiController extends Controller
         $umur = umur_geologi::find($id);
 
         return response()->json($umur, 200);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
