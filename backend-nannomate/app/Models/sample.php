6b6f8c38d685dba54db05fd23a6bdceb59c389fd
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-02-09 16:26:23 +0700

    added eloquent relationship and deleted 'id_spesies' fillable in "sample" model

--- backend-nannomate/app/Models/sample.php
+++ backend-nannomate/app/Models/sample.php
@@ -17,6 +17,21 @@ class sample extends Model
     public $timestamps = false;
     //fillable column
     protected $fillable = [
-        'id_studi_area', 'id_spesies', 'id_user', 'kode_sample', 'kelimpahan', 'preparasi', 'pengawetan', 'tujuan', 'stopsite', 'status', 'alasan', 'tanggal_dikirim', 'tanggal_diterima'
+        'id_studi_area', 'id_user', 'kode_sample', 'kelimpahan', 'preparasi', 'pengawetan', 'tujuan', 'stopsite', 'status', 'alasan', 'tanggal_dikirim', 'tanggal_diterima'
     ];
+
+    public function studiArea()
+    {
+        return $this->belongsTo(studi_area::class, 'id_studi_area', 'id_studi_area');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(user::class, 'id_user', 'id_user');
+    }
+
+    public function sampleSpesies()
+    {
+        return $this->hasMany(sample_spesies::class, 'id_sample', 'id_sample');
+    }
 }
