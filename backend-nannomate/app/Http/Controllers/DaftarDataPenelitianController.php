64eb4676ae2657063cdb09e84579a040f6c15825
RadityaCahyoAdhi <marcel1324.raditya@gmail.com>
2022-06-07 11:40:41 +0700

    added 'spesies' data to 'daftarDataPenelitian'

--- backend-nannomate/app/Http/Controllers/DaftarDataPenelitianController.php
+++ backend-nannomate/app/Http/Controllers/DaftarDataPenelitianController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\observer;
+use App\Models\sample_spesies;
+use App\Models\spesies_nanofosil;
 
 class DaftarDataPenelitianController extends Controller
 {
@@ -13,6 +15,23 @@ class DaftarDataPenelitianController extends Controller
         ->join('sample', 'sample.id_studi_area', '=', 'studi_area.id_studi_area')
         ->where('status', '=', 'diterima')
         ->get(['sample.id_sample', 'observer.nama_observer', 'studi_area.lokasi', 'studi_area.formasi', 'sample.kode_sample', 'sample.tujuan']);
+
+        // get each sample's sample_spesies
+        $i = 0;
+        foreach ($daftarDataPenelitian as $dataPenelitian) {
+            $dataSampleSpesies[$i] = sample_spesies::where('id_sample', '=', $dataPenelitian->id_sample)->get();
+            
+            // get each sample's spesies_nanofosil
+            $j = 0;
+            $dataSpesies = [];
+            foreach ($dataSampleSpesies[$i] as $dataSampleSpesiesValue) {
+                $dataSpesies[$j] = spesies_nanofosil::where('id_spesies', '=', $dataSampleSpesiesValue->id_spesies)->get()[0]->nama_spesies;
+                $j++;
+            }
+            
+            $daftarDataPenelitian[$i]->spesies = $dataSpesies;
+            $i++;
+        }
 
         return response()->json($daftarDataPenelitian, 200);
     }
