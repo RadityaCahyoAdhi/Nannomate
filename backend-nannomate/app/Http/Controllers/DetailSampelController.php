cf0c6e151a18fd28c41168f4b6d0aad326aa9c80
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-08-01 21:17:31 +0700

    updated 'DetailSampelController' 'store' function

--- backend-nannomate/app/Http/Controllers/DetailSampelController.php
+++ backend-nannomate/app/Http/Controllers/DetailSampelController.php
@@ -84,9 +84,10 @@ class DetailSampelController extends Controller
                 }
             }
 
-            //memastikan jumlah spesies telah dimasukkan untuk spesies tambahan
+            //memastikan masukan spesies tambahan beserta jumlah spesies, umur awal dan akhirnya telah sesuai
             if($request->spesies_tambahan != null) {
                 $spesies_nanofosil = explode(', ', $request->spesies_tambahan);
+                //memastikan jumlah spesies telah dimasukkan untuk spesies tambahan
                 if($request->spesies_tambahan_jumlah != null) {
                     $spesies_tambahan_jumlah = explode(', ', $request->spesies_tambahan_jumlah);
                     if(count($spesies_nanofosil) != count($spesies_tambahan_jumlah)) {
@@ -95,6 +96,97 @@ class DetailSampelController extends Controller
                 }
                 else {
                     return response()->json(['error'=> 'Ada jumlah spesies yang belum dimasukkan'], 400);
+                }
+
+                if ($request->umur_awal_tambahan != null) {
+                    $umur_awal_tambahan = explode(', ', $request->umur_awal_tambahan);
+                    //memastikan elemen-elemen umur_awal_tambahan harus berupa integer
+                    try {
+                        $error = array('umur_awal_tambahan' => ['Elemen-elemen umur_awal_tambahan harus berupa integer']);
+                        foreach($umur_awal_tambahan as $umur_awal_tambahan_value) {
+                            if(!is_integer($umur_awal_tambahan_value + 1)) {
+                                return response()->json(['error'=> $error], 400);
+                            }
+                        }
+                    }
+                    catch(\Exception $exception){
+                        return response()->json(['error'=> $error], 400);
+                    }
+
+                    //memastikan setiap elemen umur_awal_tambahan bernilai antara 1 dan 46
+                    $error = array('umur_awal_tambahan' => ['Elemen-elemen umur_awal_tambahan harus antara 1 dan 46']);
+                    foreach($umur_awal_tambahan as $umur_awal_tambahan_value) {
+                        if($umur_awal_tambahan_value < 1 || $umur_awal_tambahan_value > 46) {
+                            return response()->json(['error'=> $error], 400);
+                        }
+                    }
+                }
+
+                if ($request->umur_akhir_tambahan != null) {
+                    $umur_akhir_tambahan = explode(', ', $request->umur_akhir_tambahan);
+                    //memastikan elemen-elemen umur_akhir_tambahan harus berupa integer
+                    try {
+                        $error = array('umur_akhir_tambahan' => ['Elemen-elemen umur_akhir_tambahan harus berupa integer']);
+                        foreach($umur_akhir_tambahan as $umur_akhir_tambahan_value) {
+                            if(!is_integer($umur_akhir_tambahan_value + 1)) {
+                                return response()->json(['error'=> $error], 400);
+                            }
+                        }
+                    }
+                    catch(\Exception $exception){
+                        return response()->json(['error'=> $error], 400);
+                    }
+
+                    //memastikan setiap elemen umur_akhir_tambahan bernilai antara 1 dan 46
+                    $error = array('umur_akhir_tambahan' => ['Elemen-elemen umur_akhir_tambahan harus antara 1 dan 46']);
+                    foreach($umur_akhir_tambahan as $umur_akhir_tambahan_value) {
+                        if($umur_akhir_tambahan_value < 1 || $umur_akhir_tambahan_value > 46) {
+                            return response()->json(['error'=> $error], 400);
+                        }
+                    }
+                }
+
+                //memastikan umur awal spesies tambahan telah dimasukkan
+                if($request->umur_awal_tambahan != null) {
+                    if(count($spesies_nanofosil) != count($umur_awal_tambahan)) {
+                        return response()->json(['error'=> 'Ada umur awal spesies tambahan yang belum dimasukkan'], 400);
+                    }
+                }
+                else {
+                    return response()->json(['error'=> 'Ada umur awal spesies tambahan yang belum dimasukkan'], 400);
+                }
+
+                //memastikan umur akhir spesies tambahan telah dimasukkan
+                if($request->umur_akhir_tambahan != null) {
+                    if(count($spesies_nanofosil) != count($umur_akhir_tambahan)) {
+                        return response()->json(['error'=> 'Ada umur akhir spesies tambahan yang belum dimasukkan'], 400);
+                    }
+                }
+                else {
+                    return response()->json(['error'=> 'Ada umur akhir spesies tambahan yang belum dimasukkan'], 400);
+                }
+
+                //memastikan setiap elemen umur_akhir_tambahan lebih dari atau sama dengan setiap elemen umur_awal_tambahan nya
+                if($request->umur_awal_tambahan != null && $request->umur_akhir_tambahan != null) {
+                    for ($i=0; $i < count($umur_awal_tambahan); $i++) {
+                        if ($umur_awal_tambahan[$i] > $umur_akhir_tambahan[$i]) {
+                            return response()->json(['error'=> 'Ada umur awal melebihi umur akhir spesies tambahan'], 400);
+                        }
+                    }
+                }
+            }
+
+            //mengumpulkan data zona geologi spesies tambahan
+            if($request->spesies_tambahan != null) {
+                $zona_geologi = [];
+                if($request->umur_awal_tambahan != null && $request->umur_akhir_tambahan != null) {
+                    for ($i=0; $i < count($umur_awal_tambahan); $i++) {
+                        $zona_geologi_tambahan = [];
+                        for($j=0; $j <= ($umur_akhir_tambahan[$i] - $umur_awal_tambahan[$i]); $j++) {
+                            $zona_geologi_tambahan[$j]['id_umur'] = ($umur_awal_tambahan[$i] + $j);
+                        }
+                        $zona_geologi[$i] = $zona_geologi_tambahan;
+                    }
                 }
             }
 
@@ -152,7 +244,7 @@ class DetailSampelController extends Controller
                 }
             }
 
-            //memasukkan data spesies tambahan ke dalam table spesies_nanofosil dan sample_spesies
+            //memasukkan data spesies tambahan ke dalam table spesies_nanofosil, sample_spesies, dan zona_geologi
             if ($request->spesies_tambahan != null) {
                 $j = 0;
                 foreach ($spesies_nanofosil as $spesies_nanofosil_value) {
@@ -165,6 +257,11 @@ class DetailSampelController extends Controller
                         'id_spesies' => $id_spesies_tambahan['id_spesies'],
                         'jumlah' => trim($spesies_tambahan_jumlah[$j])
                     ]);
+                    foreach ($zona_geologi[$j] as $input_zona_geologi) {
+                        zona_geologi::create([
+                            'id_spesies' => $id_spesies_tambahan['id_spesies'],
+                            'id_umur' => $input_zona_geologi['id_umur']]);
+                        }
                     $j++;
                 }
             }
