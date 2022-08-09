c5fb22f7240a4a4726c7ca6b4bab2b975f7d644d
RadityaCahyoAdhi <raditya.c.a@mail.ugm.ac.id>
2022-08-09 15:16:52 +0700

    updated 'TerimaSampelController'

--- backend-nannomate/app/Http/Controllers/TerimaSampelController.php
+++ backend-nannomate/app/Http/Controllers/TerimaSampelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Models\sample;
+use App\Models\sample_spesies;
+use App\Models\spesies_nanofosil;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,10 +22,21 @@ class TerimaSampelController extends Controller
             if (is_null($sample)){
                 return response()->json(['error'=>'Data Not Found!'], 404);
             }
+
             sample::where('id_sample', '=', $id)->update([
                 'status' => 'diterima',
                 'tanggal_diterima' => Carbon::now()->toDateString()
             ]);
+
+            $sample_spesies = sample_spesies::where('id_sample', '=', $sample['id_sample'])->get();
+            foreach ($sample_spesies as $sample_spesies_value) {
+                $spesies = spesies_nanofosil::find($sample_spesies_value['id_spesies']);
+                if ($spesies['status'] == 'tambahan') {
+                    $spesies->update([
+                        'status' => 'terverifikasi'
+                    ]);
+                }
+            }
 
             return response()->json(['success' => 'Data successfully updated'], 200);
         }
